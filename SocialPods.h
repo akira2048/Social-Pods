c6cb54d8084ffe8f3e131e89be6cab8658ab118e
HorieTakumi <HorieTakumi@users.noreply.github.com>
2017-08-01 17:11:07 +0900

    Update SocialPods.h

--- SocialPods.h
+++ SocialPods.h
@@ -27,7 +27,7 @@
 //               Variables              //
 //======================================//
 
-extern int servo_angles[2][2][2];
+extern int servoAngles[2][2][2];
 extern int calib[2][2][2];
 extern VarSpeedServo servo[2][2][2];
 
@@ -45,6 +45,7 @@ void armMoveTo(int FR, int LR, int angle, int _speed = 50);
 void initializePose();
 
 //Low Level Function
+int readServoAngle(int FR, int LR, int SC);
 void servoMove(int FR, int LR, int SC, int delta_angle, bool isWait = true, int _speed = 50);
 void servoMove(int FR, int LR, int SC, int delta_angle, int _speed);
 void servoMoveTo(int FR, int LR, int SC, int angle, bool isWait = true, int _speed = 50);
